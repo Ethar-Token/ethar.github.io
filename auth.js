e0c92703e6a704f0c5175bbe1f3816aa6f05d55c
Ethar-Token <etharcryptoproject@gmail.com>
2026-07-13 03:05:02 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,23 @@
+const supabaseUrl = "https://ynohexdhktetiepmbwnx.supabase.co";
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlub2hleGRoa3RldGllcG1id254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3MDU5ODcsImV4cCI6MjA5OTI4MTk4N30.bOgCv0UJxYvNSXDxCFs9_6lWfm-h5RipjLzqFIC7Miw";
+
+const client = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+async function requireLogin() {
+
+    const { data, error } = await client.auth.getSession();
+
+    if (error || !data.session) {
+
+        window.location.href = "login.html";
+        return;
+
+    }
+
+    return data.session;
+
+}
