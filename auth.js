e1bbcce0aa89f1199310fa8f74b2ce986b9c4fd6
Ethar-Token <etharcryptoproject@gmail.com>
2026-07-27 17:52:02 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -765,3 +765,49 @@ async function sendTicketEmail(){
     });
 }
 
+function showToast(message, type = "success") {
+
+    const old = document.getElementById("etharToast");
+
+    if (old) {
+        old.remove();
+    }
+
+    const toast = document.createElement("div");
+
+    toast.id = "etharToast";
+
+    toast.innerHTML = `
+        <span class="toastIcon">
+            ${type === "success" ? "✔" : "✖"}
+        </span>
+
+        <span class="toastText">
+            ${message}
+        </span>
+    `;
+
+    toast.className = "etharToast";
+
+    document.body.appendChild(toast);
+
+    requestAnimationFrame(() => {
+
+        toast.classList.add("show");
+
+    });
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+        setTimeout(() => {
+
+            toast.remove();
+
+        },300);
+
+    },3000);
+
+}
+
